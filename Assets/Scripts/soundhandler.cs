df4219b49c965bb9c95de604fb1cbd191801ca6c
Prince Johnson <65087254+BlazerX88@users.noreply.github.com>
2021-03-02 01:31:38 +0530

    Add files via upload

---
+++ Assets/Scripts/soundhandler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class soundhandler : MonoBehaviour
+{
+
+    private AudioSource mysounds;
+    private AudioSource mysounds1;
+
+    private AudioSource jump;
+    private AudioSource die;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mysounds = GetComponent<AudioSource>();
+        mysounds1 = GetComponent<AudioSource>();
+
+        die = mysounds1;
+        jump = mysounds;
+       
+    }
+
+
+    public void PlayDie()
+    {
+        die.Play();
+    }
+
+
+    // Update is called once per frame
+    public void PlayJump()
+    {
+        jump.Play();
+    }
+
+}
